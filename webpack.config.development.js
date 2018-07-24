fcdabd2aed67628a95f552d29b549d481e2f435a
Cheton Wu <cheton@gmail.com>
2018-07-24 13:33:27 +0800

    Update webpack config

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,6 +1,9 @@
+process.env.NODE_ENV = 'development'; // required for the `env` option of the `.babelrc` config
+
 const path = require('path');
 const CSSSplitWebpackPlugin = require('css-split-webpack-plugin').default;
 const dotenv = require('dotenv');
+const findImports = require('find-imports');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const without = require('lodash/without');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -10,28 +13,56 @@ const stylusLoader = require('stylus-loader');
 const webpack = require('webpack');
 const WriteFileWebpackPlugin = require('write-file-webpack-plugin');
 const buildConfig = require('./build.config');
-const webpackConfigRules = require('./webpack.config.rules');
 const pkg = require('./package.json');
 
 dotenv.config();
 
 const publicPath = process.env.PUBLIC_PATH || '';
 const buildVersion = pkg.version;
+const timestamp = new Date().getTime();
 
-const webpackConfig = Object.assign({}, webpackConfigRules, {
+module.exports = {
     mode: 'development',
     cache: true,
     target: 'web',
     devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        //
+        // https://webpack.github.io/docs/webpack-dev-server.html#additional-configuration-options
+        //
+        disableHostCheck: true,
+        noInfo: false,
+        lazy: false,
+        // // https://webpack.github.io/docs/node.js-api.html#compiler
+        watchOptions: {
+            // poll: true, // use polling instead of native watchers
+            ignored: /node_modules/
+        },
+        proxy: {
+            '/api': {
+                target: process.env.PROXY_TARGET,
+                changeOrigin: true
+            }
+        }
+    },
     entry: {
         polyfill: [
-            // https://github.com/Yaffle/EventSource
-            'eventsource-polyfill',
+            'eventsource-polyfill', // https://github.com/Yaffle/EventSource
+            'webpack-hot-middleware/client?reload=true', // https://github.com/glenjamin/webpack-hot-middleware
             path.resolve(__dirname, 'app/polyfill/index.js')
         ],
+        vendor: [
+            'eventsource-polyfill', // https://github.com/Yaffle/EventSource
+            'webpack-hot-middleware/client?reload=true', // https://github.com/glenjamin/webpack-hot-middleware
+            ...findImports([
+                'app/**/*.{js,jsx}',
+                '!app/polyfill/**/*.js',
+                '!app/**/*.production.js'
+            ], { flatten: true })
+        ],
         app: [
-            // https://github.com/Yaffle/EventSource
-            'eventsource-polyfill',
+            'eventsource-polyfill', // https://github.com/Yaffle/EventSource
+            'webpack-hot-middleware/client?reload=true', // https://github.com/glenjamin/webpack-hot-middleware
             path.resolve(__dirname, 'app/index.js')
         ]
     },
@@ -40,6 +71,79 @@ const webpackConfig = Object.assign({}, webpackConfigRules, {
         filename: '[name].bundle.js',
         chunkFilename: '[name].chunk.js',
         publicPath: publicPath
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'eslint-loader',
+                enforce: 'pre',
+                exclude: /node_modules/,
+                options: {
+                    quiet: true
+                }
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|docs)/
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader?camelCase&modules&importLoaders=1&localIdentName=[path][name]__[local]--[hash:base64:5]',
+                    'stylus-loader'
+                ],
+                exclude: [
+                    path.resolve(__dirname, 'app/styles')
+                ]
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader?camelCase',
+                    'stylus-loader'
+                ],
+                include: [
+                    path.resolve(__dirname, 'app/styles')
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 8192
+                }
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: 'application/font-woff'
+                }
+            },
+            {
+                test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    node: {
+        // Some libraries import Node modules but don't use them in the browser.
+        // Tell Webpack to provide empty mocks for them so importing them works.
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
     },
     optimization: {
         minimizer: [
@@ -78,8 +182,8 @@ const webpackConfig = Object.assign({}, webpackConfigRules, {
             new RegExp('^\./(' + without(buildConfig.languages, 'en').join('|') + ')$')
         ),
         new MiniCssExtractPlugin({
-            filename: `[name].css?_=${buildVersion}`,
-            chunkFilename: `[id].css?_=${buildVersion}`,
+            filename: `[name].css?_=${timestamp}`,
+            chunkFilename: `[id].css?_=${timestamp}`
         }),
         new CSSSplitWebpackPlugin({
             size: 4000,
@@ -91,34 +195,13 @@ const webpackConfig = Object.assign({}, webpackConfigRules, {
             filename: path.resolve(__dirname, 'output/index.html'),
             template: path.resolve(__dirname, 'public/index.html'),
             chunksSortMode: 'dependency' // Sort chunks by dependency
-        }),
-    ].filter(Boolean), // filter out empty values
-    node: {
-        // Some libraries import Node modules but don't use them in the browser.
-        // Tell Webpack to provide empty mocks for them so importing them works.
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
-    },
-    devServer: {
-        //
-        // https://webpack.github.io/docs/webpack-dev-server.html#additional-configuration-options
-        //
-        disableHostCheck: true,
-        noInfo: false,
-        lazy: false,
-        // // https://webpack.github.io/docs/node.js-api.html#compiler
-        watchOptions: {
-            // poll: true, // use polling instead of native watchers
-            ignored: /node_modules/
-        },
-        proxy: {
-            '/api': {
-                target: process.env.PROXY_TARGET,
-                changeOrigin: true
-            }
-        }
+        })
+    ],
+    resolve: {
+        modules: [
+            path.resolve(__dirname),
+            'node_modules'
+        ],
+        extensions: ['.js', '.jsx']
     }
-});
-
-module.exports = webpackConfig;
+};
